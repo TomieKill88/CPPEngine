d00f96b223ee73a08c0d7e6e4191f12e9d131eac
TomieKill88 <lordvaldomero1988@gmail.com>
2025-08-03 20:06:54 +0200

    - Changed variables for lists when adding sources - Added path.cmake with the path to sources and external

---
+++ source/cmake/paths/paths.cmake
@@ -0,0 +1,2 @@
+set(LIBRARIES_DIRECTORY "${PROJECT_SOURCE_DIR}/src" CACHE PATH "Path to projects own libraries")
+set(EXT_LIBRARIES_DIRECTORY "C:/Repositories" CACHE PATH "Path to external libraries")
