5584a1ac1cec0c4181a998214b8ec4a633b9e1f2
TomieKill88 <lordvaldomero1988@gmail.com>
2025-08-05 21:18:04 +0200

    - Install step added, but it has to be manually build - Install paths added to cache

--- source/cmake/paths/paths.cmake
+++ source/cmake/paths/paths.cmake
@@ -1,2 +1,7 @@
 set(LIBRARIES_DIRECTORY "${PROJECT_SOURCE_DIR}/src" CACHE PATH "Path to projects own libraries")
 set(EXT_LIBRARIES_DIRECTORY "C:/Repositories" CACHE PATH "Path to external libraries")
+
+set(INSTALL_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}/target" CACHE PATH "Install directory for project")
+set(INSTALL_BINDIR "${INSTALL_DIRECTORY}/bin" CACHE PATH "Install directory for project binaries")
+set(INSTALL_LIBDIR "${INSTALL_DIRECTORY}/lib" CACHE PATH "Install directory for project libs")
+set(INSTALL_INCLUDEDIR "${INSTALL_DIRECTORY}/include" CACHE PATH "Install directory for project public header")
