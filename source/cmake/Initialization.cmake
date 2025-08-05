1454d5b65f50035364d46739abc536595767c248
TomieKill88 <lordvaldomero1988@gmail.com>
2025-08-05 14:40:54 +0200

    small changes added

--- source/cmake/Initialization.cmake
+++ source/cmake/Initialization.cmake
@@ -1,5 +1,13 @@
 set(CMAKE_CXX_STANDARD 17)
 set(CMAKE_CXX_STANDARD_REQUIRED true)
 
+#--------------------------------------------------------------------------
+# Include CMake Modules
+#--------------------------------------------------------------------------
+include(GNUInstallDirs)
+
+#--------------------------------------------------------------------------
+# Include OWN CMake Modules
+#--------------------------------------------------------------------------
 list(APPEND CMAKE_MODULE_PATH ${CMAKE_CURRENT_LIST_DIR}/paths)
 include(paths)
