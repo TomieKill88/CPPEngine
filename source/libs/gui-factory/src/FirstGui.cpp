eab83d08c4a6ee0e2233dc6b9148853f61b60601
TomieKill88 <lordvaldomero1988@gmail.com>
2025-11-11 21:13:28 +0100

    gui-factory for own GUI arhitectures created

---
+++ source/libs/gui-factory/src/FirstGui.cpp
@@ -0,0 +1,6 @@
+#include "FirstGui.hpp"
+
+namespace CustomGui
+{
+	
+}
---
+++ source/libs/gui-factory/src/FirstGui.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <memory>
+
+#include <SFML/Graphics.hpp>
+
+namespace CustomGui
+{
+	class FirstGui
+	{
+	private:
+
+	public:
+
+		FirstGui() = default;
+		virtual ~FirstGui() = default;
+	};
+}
